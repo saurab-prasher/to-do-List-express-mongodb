181f301bc971644187405335afc7b892e21a8f02
Saurab Prasher <saurab015@gmail.com>
2021-01-04 14:55:16 +0530

    styles added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.get("/", (req, res) => {
-  res.send("It worked!");
+  res.render("todo");
 });
 
 app.listen(3000, () => {
