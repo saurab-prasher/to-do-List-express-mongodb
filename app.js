5024c83789da76bfeb9de3dd615a3686653ed6d5
Saurab Prasher <saurab015@gmail.com>
2021-01-15 13:43:12 +0530

    some tweaks

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const todo = require("./models/tasks");
 
 app.set("view engine", "ejs");
 
@@ -14,6 +15,8 @@ mongoose
   .connect("mongodb://localhost:27017/todo-list", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("MONGO CONNECTION OPEN!!!");
@@ -23,17 +26,12 @@ mongoose
     console.log(err);
   });
 
-const todoSchema = new mongoose.Schema({
-  task: {
-    type: String,
-    required: true,
-  },
+app.get("/home", (req, res) => {
+  res.render("home");
 });
 
-const Todo = mongoose.model("Todo", todoSchema);
-
 app.get("/", async (req, res) => {
-  const task = await Todo.find({});
+  const task = await todo.find({});
   res.render("todo", { task });
 });
 
@@ -41,7 +39,7 @@ app.get("/", async (req, res) => {
 app.post("/", async (req, res) => {
   try {
     const task = req.body;
-    const newTask = new Todo({ task: task.content });
+    const newTask = new todo({ task: task.task });
     await newTask.save();
     res.redirect("/");
   } catch (err) {
@@ -49,22 +47,23 @@ app.post("/", async (req, res) => {
   }
 });
 
+// Edit Task render form
 app.get("/:id/edit", async (req, res) => {
   try {
-    const id = req.params.id;
-    const task = await Todo.find({});
-    res.render("todoEdit", { task, idTask: id });
+    const task = await todo.findOne({ _id: req.params.id });
+    res.render("todoEdit", { task });
   } catch (err) {
     res.send("something went wrong");
   }
 });
 
-app.put("/edit/:id", async (req, res) => {
+// Edit task handle post request
+app.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const newTask = await Todo.findByIdAndUpdate(
+    const newTask = await todo.findByIdAndUpdate(
       id,
-      { task: req.body.content },
+      { task: req.body.task },
       { runValidators: true }
     );
     res.redirect("/");
@@ -73,10 +72,11 @@ app.put("/edit/:id", async (req, res) => {
   }
 });
 
+// Delete task
 app.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const deleteTask = await Todo.findByIdAndDelete(id);
+    const deleteTask = await todo.findByIdAndDelete(id);
     res.redirect("/");
   } catch (err) {
     res.send("something went wrong");
